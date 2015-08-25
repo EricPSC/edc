d3fb750bdf60d1d3d1742a0d7d87b98ba0a87fa1
Eric <ericcesena@outlook.com>
2015-08-25 10:37:15 -0700

    first one of many

---
+++ aug24-slate/js/script.js
@@ -0,0 +1,51 @@
+// put your javascript here
+
+
+// $(document).ready(function() {
+//   $("#cf7_controls").on('click', 'span', function() {
+//     $("#cf7 img").removeClass("opaque");
+
+//     var newImage = $(this).index();
+
+//     $("#cf7 img").eq(newImage).addClass("opaque");
+
+//     $("#cf7_controls span").removeClass("selected");
+//     $(this).addClass("selected");
+//   });
+// });
+
+
+// var $btn = document.getElementsByTagName('button')[0],
+// 	$img = document.getElementById('cf7'),
+// 	$num = 1;
+	
+// $btn.addEventListener('click', addClassName);
+	
+// function addClassName(){
+// 	$img.classList.toggle('active');
+// 	// console.log($num);
+// 	// $num = Math.floor( (Math.random() * 8) + 1);
+// 	// console.log($num);
+// 	 $img.className = "";
+// 	$img.classList.add("img-" + $num);
+// };	
+
+// var $btns = document.getElementById('cycle-btns'),
+//     $imgs = document.getElementsByClassName('cycle-imgs');
+
+// function thisEl(el){
+//     $btns.children[el].addEventListener('click', function(){
+//         for(var $i = 0; $i < $imgs.length; $i++){
+//             $imgs[$i].classList.remove('active');
+//         }
+//         $imgs[el].classList.add('active');
+//     });
+// }
+
+// for(var $i = 0, len = $btns.children.length; $i < len; $i++){
+//     thisEl($i);
+// }
+
+
+
+
